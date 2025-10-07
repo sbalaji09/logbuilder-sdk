fd2e003a7ff136e3b462d0ebec1b2e24bdc252a9
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-10-06 17:02:50 -0700

    completed transport.ts to send logs to the backend server

---
+++ client/transport.ts
@@ -0,0 +1,28 @@
+import { LogEntry } from "./types";
+
+class Transport {
+    endpointURL: string;
+
+    constructor(endpointURL: string) {
+        this.endpointURL = endpointURL;
+    }
+
+    async send(logs: LogEntry[]) : Promise<{ success: boolean; error?: string }>{
+        try {
+            const response = await fetch('https://placeholderbackend.com/logs', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(logs),
+              });
+          
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return { success: true };
+        } catch (error) {
+            return { success: false, error: error?.message || "Unknown error" };
+        }
+    }
+}
